8719f376a7debfff9a899ce8a17ab00e948708e6
Richard Lock <lock.j.h@gmail.com>
2018-10-31 21:46:03 -0500

    [store/index.js] In redux, the data is stored in a giant store instead of multiple component states. To create the store we utilize the createStore Redux method. We also import compose method which is used to enhance stores. We also import the connectRouter method to help connect our router (which will be ConnectedRouter instead of BrowserRouter) with Redux. For that we also need the history object that we created earlier. createStore method also requires the rootReducer which is a distilled reducer that combines all other reducers. We also need all the data that we will be using.
    We also utilize the applyMiddleware. Middleware provides a way to interact with actions that have been dispatched to the store before they reach the store's reducer. Examples of different uses for middleware include logging actions, reporting errors, making asynchronous requests, and dispatching new actions.

---
+++ learn-redux/client/stores/index.js
@@ -0,0 +1,26 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+
+// hook up our Router aka ConnectedRouter with Redux using the connectRouter method and the history object
+import { connectRouter, routerMiddleware } from 'connected-react-router';
+import history from './history';
+
+// import the root reducer
+import rootReducer from './reducers/index';
+
+// import data
+import comments from './../data/comments';
+import posts from './../data/posts';
+
+// create an object for the default data
+const defaultState = {
+  posts,
+  comments,
+};
+
+const store = createStore(
+  connectRouter(history)(rootReducer),
+  defaultState,
+  compose(applyMiddleware(routerMiddleware(history))),
+);
+
+export default store;
